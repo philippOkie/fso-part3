f84e3448447530b2348935c09b6144fc6361debf
philippOkie <kriukphilipp@gmail.com>
2023-11-14 17:45:38 +0200

    add front to back

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ morgan.token("type", function (req, res) {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("dist"));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :type")
 );
